f2f564ce69c3bc64bd34355656c486ce7da0f2be
Ewoud Wortelboer <ew_1988@live.nl>
2016-10-19 16:22:11 +0200

    Update TableViewController

--- BarFinder/TableViewController.swift
+++ BarFinder/TableViewController.swift
@@ -10,4 +10,18 @@ import UIKit
 
 class TableViewController: UITableViewController  {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
 }
