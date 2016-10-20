d9f3231e48a27fcd4e273c2fa34241035ad2e514
Ewoud Wortelboer <ew_1988@live.nl>
2016-10-20 15:11:59 +0200

    Update on indexPath for TableViewController

--- BarFinder/Bar.swift
+++ BarFinder/Bar.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class Bar: NSObject {
+class Bar {
     
-    var barName : String = " "
-    var barAddress : String = " "
-    var barZipCode : String = " "
-    var barTimeSchedule : String = " "
+//    var barName : String = " "
+//    var barAddress : String = " "
+//    var barZipCode : String = " "
+//    var barTimeSchedule : String = " "
+    
+    var barArray = [Dictionary<String, String>]()
+    
+    func createDicts() {
+    
+    let barDict1 = ["Name" : "Bar One", "Address" : "Address One", "Zip code" : "Zip code one", "Time schedule" : "Time schedule one"]
+    
+    let barDict2 = ["Name" : "Bar Two", "Address" : "Address Two", "Zip code" : "Zip code two", "Time schedule" : "Time schedule two"]
+    
+    let barDict3 = ["Name" : "Bar Three", "Address" : "Address Three", "Zip code" : "Zip code three", "Time schedule" : "Time schedule three"]
+    
+    barArray = [barDict1, barDict2, barDict3]
+    
+    }
+    
+    func giveBarArray() -> [Dictionary<String, String>] {
+        return barArray
+    }
     
 }
