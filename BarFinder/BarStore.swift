8159395ce62974558c4bb914c8a93b1a6088f717
Michael  de Boer <michael-deboer@live.nl>
2016-10-24 17:52:28 +0200

    Upload to FireBase

--- BarFinder/BarStore.swift
+++ BarFinder/BarStore.swift
@@ -8,12 +8,15 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 class BarStore {
     
     var allBars : [Bar] = []
+    var detailBar : Bar?
     
-
+    var ref: FIRDatabaseReference!
+    
     
     func createBar() {
         
@@ -22,27 +25,34 @@ class BarStore {
         let barDict3 = Bar(name: "bar3", address: "adress 3")
         let barDict4 = Bar(name: "bar4", address: "adress 4")
         let barDict5 = Bar(name: "bar5", address: "adress 5")
-
         
         
         allBars = [barDict1, barDict2, barDict3, barDict4, barDict5]
+        
         
     }
     
     func giveBarArray() -> [Bar] {
         return allBars
     }
-    
-
-    
-//    func createBar() -> Bar {
-//        let newBar = Bar(name: name, address: address)
-//        
-//        allBars.append(newBar)
-//        
-//        return newBar
-//    }
-
+    func uploadTo(bar: Bar)  {
+        ref = FIRDatabase.database().reference()
         
+        let name = bar.name
+        let address = bar.address
+        let barRef = ref.child("Bars").childByAutoId().setValue(["Name" : name, "Address" : address])
+      //  let refName = barDictRef.
+        
+    }
+    
+    //    func createBar() -> Bar {
+    //        let newBar = Bar(name: name, address: address)
+    //
+    //        allBars.append(newBar)
+    //
+    //        return newBar
+    //    }
+    
+    
     
 }
